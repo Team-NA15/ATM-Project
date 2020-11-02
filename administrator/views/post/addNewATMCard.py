a99fc2d431ed26259ac393330841c91314c9ed0b
BrandonCorn <corn_brandon@columbusstate.edu>
2020-11-02 16:45:06 -0500

    modify base.html files remove unused css file and modify comments in routes used for examples

--- administrator/views/post/addNewATMCard.py
+++ administrator/views/post/addNewATMCard.py
@@ -14,27 +14,28 @@ def getExpireDate(date):
 def addNewATMCard(request):
     #HARDCODED EXAMPLE 
     if request.method == 'POST': 
-        card_num = randint(1000000000000000,9999999999999999)
-        account = AccountExtension.objects.get(account_number = 1111111111)
+        # card_num = randint(1000000000000000,9999999999999999)
+        # account = AccountExtension.objects.get(account_number = 1111111111)
         
-        card = ATMCard(
-            card_number = card_num,
-            pin = 5555, 
-            name = 'brandon corn', 
-            date_issued = date.today(), 
-            expire_date = getExpireDate(date.today()),
-            address = '111 choochoo lane', 
-            two_fact_auth_status = 'on', 
-            phone_number = '111-2222', 
-            card_status = 'deactivated'
-        )
-        # FOREIGN KEYS MUST BE SET AFTER THE CREATION OF THE OBJECT
-        card.account_number = account.account_number
-        print('card-number: ', card.card_number, '\naccount_number: ', card.account_number)
-        # THEN WE CAN SAVE
-        card.save()
+        # card = ATMCard(
+        #     card_number = card_num,
+        #     pin = 5555, 
+        #     name = 'brandon corn', 
+        #     date_issued = date.today(), 
+        #     expire_date = getExpireDate(date.today()),
+        #     address = '111 choochoo lane', 
+        #     two_fact_auth_status = 'on', 
+        #     phone_number = '111-2222', 
+        #     card_status = 'deactivated'
+        # )
+        # # FOREIGN KEYS MUST BE SET AFTER THE CREATION OF THE OBJECT
+        # card.account_number = account.account_number
+        # print('card-number: ', card.card_number, '\naccount_number: ', card.account_number)
+        # # THEN WE CAN SAVE
+        # card.save()
         # HERE IS AN EXAMPLE OF RETRIEVING AN ATMCARD BY CARD NUMBER
         newCard = ATMCard.objects.get(card_number = 3887665486623486)
         print(newCard)
-        return HttpResponse(card)
+        # print('cardNumber: ', newCard.card_number, '\naccount_number: ', newCard.account_number)
+        return HttpResponse(newCard)
     return HttpResponse('An error occurred')
--- user/views/get/home.py
+++ user/views/get/home.py
@@ -3,6 +3,10 @@ from user.models import AccountExtension
 
 #GET request loads page with options for ATM cardholder 
 def home(request): 
+
+    return render(request, 'user/home.html')
+
+
     #HARDCODED EXAMPLE OF MODEL CREATION
     # acc = AccountExtension(
     #     account_number = 2222222222,
@@ -16,4 +20,3 @@ def home(request):
     # for account in accounts: 
     #     print("account num: ",account.account_number)
     #     print("name: ", account.name)
-    return render(request, 'user/home.html')
