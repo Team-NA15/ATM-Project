202db22c48746b0904c891c2faaf6df0518be909
xiiethan <ethan_dillinger458@yahoo.com>
2020-11-04 19:52:45 +0000

    form change

--- user/forms.py
+++ user/forms.py
@@ -11,10 +11,6 @@ class UserForm(forms.Form):
     )
 
 class WithdarawalForm(forms.Form):
-    card_number = forms.CharField(
-        label = 'card number',
-        max_length = 16
-    )
     amount = forms.FloatField(
         label = 'amount',
         max_length = 7
