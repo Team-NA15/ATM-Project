699e5ded76e0722071975409efed56655ee2788e
BrandonCorn <corn_brandon@columbusstate.edu>
2020-11-03 20:16:31 -0500

    example query ATMCard and how to access properties

--- user/views/balanceInquiry.py
+++ user/views/balanceInquiry.py
@@ -10,9 +10,11 @@ def balanceInquiry(request):
     #TEST
     #query to retrieve ATMCard object 
     card = ATMCard.objects.get(card_number = request.session['token'])
+    
     #since this is a foreign key, card.account_number is the Account Extension Object
     #associated with this ATMCard 
     account = card.account_number
+
     #here we can access the actual account_number and balance
     print('account num: ',account.account_number)
     return HttpResponse(account.balance)
