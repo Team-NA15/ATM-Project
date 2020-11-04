f27ff9a840513150d877a51bd53106ddc988712d
BrandonCorn <corn_brandon@columbusstate.edu>
2020-11-03 20:14:46 -0500

    example how to work with ATMCard and AccountExtension Objects and querying

--- user/views/balanceInquiry.py
+++ user/views/balanceInquiry.py
@@ -1,7 +1,18 @@
+from django.shortcuts import render 
+from django.http import HttpResponse
 from ..decorator import user_authenticated
+from user.models import ATMCard
 
 #GET request for loading page to check balance of account
 @user_authenticated
 def balanceInquiry(request): 
     #logic for loading page goes here 
-    return 
+    #TEST
+    #query to retrieve ATMCard object 
+    card = ATMCard.objects.get(card_number = request.session['token'])
+    #since this is a foreign key, card.account_number is the Account Extension Object
+    #associated with this ATMCard 
+    account = card.account_number
+    #here we can access the actual account_number and balance
+    print('account num: ',account.account_number)
+    return HttpResponse(account.balance)
