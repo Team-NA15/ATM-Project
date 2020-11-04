5e3014af055bd7e46543dc96dd2b64ec1d61c914
xiiethan <ethan_dillinger458@yahoo.com>
2020-11-04 19:31:09 +0000

    Cash-withdrawal

--- user/views/cashWithdrawal.py
+++ user/views/cashWithdrawal.py
@@ -2,7 +2,7 @@ from ..decorator import user_authenticated
 from django.shortcuts import render 
 from django.http import HttpResponse
 from user.models import ATMCard
-from ..forms import UserForm
+from ..forms import WithdarawalForm
 
 #GET request to load page with form for withdrawing money 
 @user_authenticated
@@ -14,12 +14,11 @@ def cashWithdrawal(request):
         except:
             print("card not valid")
         account = card.account_number
-        form = UserForm(request.POST)
-        #Link frontend "amount entered" to withdrawal var
-        withdrawal = 0.0
+        form = WithdarawalForm(request.POST)
+        #Link frontend "amount entered" to amount var
         #Update Database
-        print('Money dispursed. New Balance: ', account.balance - withdrawal)
+        print('Money dispursed. New Balance: ', account.balance - amount)
         return HttpResponse(account.balance)
     else:
-        form = UserForm()
+        form = WithdarawalForm()
         return render(request, 'user/cardholder-login.html', {'form': form})
