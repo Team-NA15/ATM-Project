9be4ca697b6cd99b319d2e6f9b2f9fae35568c7d
xiiethan <ethan_dillinger458@yahoo.com>
2020-11-04 19:27:41 +0000

    Cash-withdrawal

--- user/forms.py
+++ user/forms.py
@@ -9,3 +9,13 @@ class UserForm(forms.Form):
         label = 'PIN', 
         max_length = 4
     )
+
+class WithdarawalForm(forms.Form):
+    card_number = forms.CharField(
+        label = 'card number',
+        max_length = 16
+    )
+    amount = forms.FloatField(
+        label = 'amount',
+        max_length = 7
+    )
--- user/views/cashWithdrawal.py
+++ user/views/cashWithdrawal.py
@@ -1,7 +1,25 @@
 from ..decorator import user_authenticated
+from django.shortcuts import render 
+from django.http import HttpResponse
+from user.models import ATMCard
+from ..forms import UserForm
 
 #GET request to load page with form for withdrawing money 
 @user_authenticated
 def cashWithdrawal(request): 
     #logic for loading page for money withdrawal 
-    return 
+    if (request.method == 'POST'):
+        try:
+            card = ATMCard.objects.get(card_number = request.session['token'])
+        except:
+            print("card not valid")
+        account = card.account_number
+        form = UserForm(request.POST)
+        #Link frontend "amount entered" to withdrawal var
+        withdrawal = 0.0
+        #Update Database
+        print('Money dispursed. New Balance: ', account.balance - withdrawal)
+        return HttpResponse(account.balance)
+    else:
+        form = UserForm()
+        return render(request, 'user/cardholder-login.html', {'form': form})
