987caa6e0d2b6878dab2793dcfcfbfcef3139ba9
xiiethan <ethan_dillinger458@yahoo.com>
2020-11-12 19:47:59 +0000

    a small change

--- user/forms.py
+++ user/forms.py
@@ -11,9 +11,8 @@ class UserForm(forms.Form):
     )
 
 class WithdrawalForm(forms.Form):
-    amount = forms.IntField(
-        label = "amount",
-        max_length = 9
+    amount = forms.IntegerField(
+        label = "amount"
     )
 
 class TransferForm(forms.Form):
