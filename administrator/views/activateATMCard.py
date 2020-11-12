792878aa7f58ae8708e7f0934a3edc9ebe155be9
BrandonCorn <corn_brandon@columbusstate.edu>
2020-11-12 18:03:07 -0500

    completed activate atm card

--- administrator/views/activateATMCard.py
+++ administrator/views/activateATMCard.py
@@ -1,4 +1,44 @@
+from django.shortcuts import render
+from ..forms import ModCardStatusForm
+from user.models import ATMCard
+
 #Request to activate an ATM card
 def activateATMCard(request): 
-    #logic for activating ATM card goes here 
+    if request.method == 'POST': 
+        form = ModCardStatusForm(request.POST)
+        if form.is_valid():
+            try: 
+                card = ATMCard.objects.get(card_number = form.cleaned_data['card_number'])
+            except: 
+                form = ModCardStatusForm()
+                return render(request, 'administrator/activate-atm-card.html', {
+                    'form': form, 
+                    'message': 'Card is invalid'
+                })
+            form = ModCardStatusForm()
+            if card.card_status == 'active': 
+                return render(request, 'administrator/activate-atm-card.html', {
+                    'form': form, 
+                    'message': 'Card is already active'
+                })
+            else: 
+                card.card_status = 'active'
+                card.save()
+                return render(request, 'administrator/activate-atm-card.html', {
+                    'form': form, 
+                    'message': 'Card has been activated'
+                })
+                
+        else: 
+            form = ModCardStatusForm()
+            return render(request, 'administrator/activate-atm-card.html', {
+                'form': form, 
+                'message': 'Form is invalid'
+            })
+    else: 
+        print(request.path)
+        form = ModCardStatusForm()
+        return render(request, 'administrator/activate-atm-card.html', {
+            'form': form
+        })
     return 
