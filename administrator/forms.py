0f8b0c801ee3dbae9ce2e9a8177be5e0ae9fdb4b
BrandonCorn <corn_brandon@columbusstate.edu>
2020-11-12 20:19:25 -0500

    finished phone number reset

--- administrator/forms.py
+++ administrator/forms.py
@@ -5,3 +5,13 @@ class ModCardForm(forms.Form):
         label = 'Card Number', 
         max_length = 16
     )
+
+class PhoneResetForm(forms.Form): 
+    card_number = forms.CharField(
+        label = 'Card Number', 
+        max_length = 16
+    )
+    phone_number = forms.CharField(
+        label = 'New Phone Number', 
+        max_length = 12
+    )
--- administrator/views/resetPhoneNumber.py
+++ administrator/views/resetPhoneNumber.py
@@ -1,4 +1,40 @@
+from django.shortcuts import render
+from ..forms import PhoneResetForm
+from user.models import ATMCard
+from main.services import renderPage
+import re
+
 #Request to reset phone number
 def resetPhoneNumber(request): 
-    #logic for reseting phone number goes here 
-    return 
+    renderData = {
+        'request': request,
+        'path': 'administrator/reset-phone-number.html',
+        'context': { 
+            'form': PhoneResetForm(),
+            'message': '',
+        } 
+    }
+    if request.method == 'POST': 
+        form = PhoneResetForm(request.POST)
+        
+        if form.is_valid():
+            try: 
+                card = ATMCard.objects.get(card_number = form.cleaned_data['card_number'])
+            except: 
+                renderData['context']['message'] = 'Card Number Invalid'
+                return renderPage(renderData)
+
+            check = form.cleaned_data['phone_number'].replace('-', '')
+            if (check.isdigit()):
+                print(form.cleaned_data['phone_number'])
+                card.phone_number = form.cleaned_data['phone_number']
+                card.save()
+                renderData['context']['message'] = 'Phone Number changed to ' + card.phone_number
+                return renderPage(renderData)               
+
+            else:
+                renderData['context']['message'] = 'Invalid Phone Number'
+                return renderPage(renderData)
+
+    else: 
+        return renderPage(renderData) 
